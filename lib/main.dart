602b8241462cfeb309c46876e53f898dc4bb6061
Zero0242 <pedrocorream66@gmail.com>
2025-04-08 15:58:07 -0400

    [PLUGINS]
    > configuracion de paquetes adicionales

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 import 'config/config.dart';
 
 void main() async {
   await GetStorage.init();
-  setPathUrlStrategy();
+  usePathUrlStrategy();
   runApp(const ProviderScope(child: MainApp()));
 }
 
@@ -21,6 +22,14 @@ class MainApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       title: "Dashboard",
       routerConfig: appRouter,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('es'),
+      ],
     );
   }
 }
